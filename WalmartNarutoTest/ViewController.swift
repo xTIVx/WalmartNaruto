539cfd9e27e59cfcda53f6387c99c13a8ae0e55f
Igor Chernobai <tiv.iosdeveloper@gmail.com>
2021-07-27 10:20:56 -0700

    Initial Commit

---
+++ WalmartNarutoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalmartNarutoTest
+//
+//  Created by Igor Chernobai on 7/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
